e2a1f5cdac55339280adce458e00a38a9815004a
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2020-10-01 13:38:20 +0200

    correct connect/disconnect

--- header.php
+++ header.php
@@ -1,11 +1,9 @@
 <?php
 
 use Base\Profil;
-
 include 'src/Base.php';
 $user = new Base\profil_utilisateurs();
 ?>
-
 <head>
     <link rel="stylesheet" href="styles/css/headerfooter.css">
 </head>
@@ -13,62 +11,39 @@ $user = new Base\profil_utilisateurs();
     <div class="big-header">
         <div class="inline-nav-img">
             <nav class="big-nav">
-                <h1 class="title">FOO2F<i class="far fa-futbol"></i>OT</h1>
-                <!--- <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_%C3%89quipe_France_Football_2018.svg/679px-Logo_%C3%89quipe_France_Football_2018.svg.png"
-                     width="auto" height="100px" alt="logo"> --->
+                <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_%C3%89quipe_France_Football_2018.svg/679px-Logo_%C3%89quipe_France_Football_2018.svg.png"
+                     width="auto" height="100px" alt="logo">
                 <ul>
-                    <div class="dropdown">
-                        <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"> <a href=""> <i class="fas fa-user"></i></a></button>
-                        <div class="dropdown-menu">
-                            <a class="dropdown-item" href="connexion.php">Se connecter</a>
-                            <a class="dropdown-item" href="inscription.php">S'inscrire</a>
-                            <a class="dropdown-item" href="#"></a>
-                        </div>
-                    </div>
+                    <li><a href=""> <i class="fas fa-user"></i></a></li>
                     <li><a href=""> <i class="fas fa-cart-plus"></i></a></li>
                 </ul>
-                <?php if (isset($_SESSION['id'])) { ?>
-                    <ul>
-                        <div class="dropdown">
-                            <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"> <a href=""> <i class="fas fa-user"></i></a></button>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="moncompte.php">Mon compte</a>
-                                <a class="dropdown-item" href="connexion.php">Déconnexion</a>
-                                <a class="dropdown-item" href="#"></a>
-                            </div>
-                        </div>
-                        <li><a href=""> <i class="fas fa-cart-plus"></i></a></li>
-                    </ul>
-                <?php
-                }
-                ?>
             </nav>
         </div>
         <div class="list-header">
             <ul>
                 <li><a href="">
-                        Accueil
+                        jqdsjbqsdjsqdbj
                     </a>
                 </li>
                 <li><a href="">
-                        Boutique
+                        jqdsjbqsdjsqdbj
                     </a>
                 </li>
                 <li><a href="">
-                        Calendrier
+                        jqdsjbqsdjsqdbj
                     </a>
+                </li>
+                <li> <?php if ($user->getIsConnect()) { ?>
+                    Bonjour <?= $user->prenom ?></li>
+                <?php } else { ?>
+                    <li><a href="connexion.php">Connexion</a></li>
+                    <li><a href="inscription.php">Inscription</a></li>
+                <?php } ?>
                 </li>
             </ul>
-
             <form class="form-inline">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-dark" type="submit">Search</button>
             </form>
         </div>
-        <?php if ($user->getIsConnect()) { ?>Bonjour <?= $user->prenom ?></li>
-    <?php } else {
-            return false;
-        }
-    ?>
-    </div>
 </header>
