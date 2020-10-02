4d0b5603c4d3ad8fe221d7a8dc5595632c075b23
lea-martel <lea.martel@laplateforme.io>
2020-10-02 18:37:09 +0200

    css index

--- footer.php
+++ footer.php
@@ -1,8 +1,27 @@
 <footer>
+<div class="list-header">
+            <ul>
+                <li><a href="">
+                        Accueil
+                    </a>
+                </li>
+                <li><a href="">
+                        Boutique
+                    </a>
+                </li>
+                <li><a href="">
+                       Calendrier
+                    </a>
+                </li>
     <?php if (!isset($_SESSION['id'])) { ?>
         <a href="inscription.php">Inscription</a>
         <a href="connexion.php">Connexion</a>
     <?php } else { ?>
         <a href="disconnect.php">Déconnexion</a>
     <?php } ?>
+    <div class="social">
+    <li><a href=""> <i class="fab fa-instagram"></i></a></li>
+    <li><a href=""> <i class="fab fa-facebook"></i></a></li>
+    <li><a href=""> <i class="fab fa-twitter"></i></a></li>
+
 </footer>
