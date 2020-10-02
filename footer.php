af695a9ecdaadd4c2758ca5735c4d56dce66d36d
lea-martel <lea.martel@laplateforme.io>
2020-10-02 18:40:29 +0200

    footer

--- footer.php
+++ footer.php
@@ -23,5 +23,5 @@
     <li><a href=""> <i class="fab fa-instagram"></i></a></li>
     <li><a href=""> <i class="fab fa-facebook"></i></a></li>
     <li><a href=""> <i class="fab fa-twitter"></i></a></li>
-
+</div>
 </footer>
