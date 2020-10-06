0f94eae09006ec17b1cd79abd441503197f61f33
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2020-10-06 14:20:08 +0200

    correct header.php

--- header.php
+++ header.php
@@ -4,6 +4,7 @@ use Base\Profil;
 include 'src/Base.php';
 $user = new Base\profil_utilisateurs();
 ?>
+
 <head>
     <link rel="stylesheet" href="styles/css/headerfooter.css">
 </head>
@@ -11,10 +12,11 @@ $user = new Base\profil_utilisateurs();
     <div class="big-header">
         <div class="inline-nav-img">
             <nav class="big-nav">
-                <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_%C3%89quipe_France_Football_2018.svg/679px-Logo_%C3%89quipe_France_Football_2018.svg.png"
-                     width="auto" height="100px" alt="logo">
+                <h1 class="title">FOO2F<i class="far fa-futbol"></i>OT</h1>
+                <!--- <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/4/43/Logo_%C3%89quipe_France_Football_2018.svg/679px-Logo_%C3%89quipe_France_Football_2018.svg.png"
+                     width="auto" height="100px" alt="logo"> --->
                 <ul>
-                    <li><a href=""> <i class="fas fa-user"></i></a></li>
+                    <li><a href="inscription.php"><i class="fas fa-user"></i></a></li>
                     <li><a href=""> <i class="fas fa-cart-plus"></i></a></li>
                 </ul>
             </nav>
@@ -22,28 +24,30 @@ $user = new Base\profil_utilisateurs();
         <div class="list-header">
             <ul>
                 <li><a href="">
-                        jqdsjbqsdjsqdbj
+                        Accueil
                     </a>
                 </li>
                 <li><a href="">
-                        jqdsjbqsdjsqdbj
+                        Boutique
                     </a>
                 </li>
                 <li><a href="">
-                        jqdsjbqsdjsqdbj
+                        Calendrier
                     </a>
-                </li>
-                <li> <?php if ($user->getIsConnect()) { ?>
-                    Bonjour <?= $user->prenom ?></li>
-                <?php } else { ?>
-                    <li><a href="connexion.php">Connexion</a></li>
-                    <li><a href="inscription.php">Inscription</a></li>
-                <?php } ?>
                 </li>
             </ul>
+
             <form class="form-inline">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-dark" type="submit">Search</button>
             </form>
         </div>
+        <?php if ($user->getIsConnect())
+                 { ?>Bonjour <?= $user->prenom ?></li> 
+                 <?php }
+                  else { 
+                      return false; 
+                  } 
+                  ?>
+    </div>
 </header>
