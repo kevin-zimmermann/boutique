edba3b313576437f3fb25626d76d108190eb1d9c
lea-martel <lea.martel@laplateforme.io>
2020-10-12 16:33:22 +0200

    modif css header

--- footer.php
+++ footer.php
@@ -16,8 +16,6 @@
     <?php if (!isset($_SESSION['id'])) { ?>
         <a href="inscription.php">Inscription</a>
         <a href="connexion.php">Connexion</a>
-    <?php } else { ?>
-        <a href="disconnect.php">Déconnexion</a>
     <?php } ?>
     <div class="social">
     <li><a href=""> <i class="fab fa-instagram"></i></a></li>
