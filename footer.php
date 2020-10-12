f9bd82146e3e1af03f0417af3dc77953f9224f2a
lea-martel <lea.martel@laplateforme.io>
2020-10-12 16:33:22 +0200

    modif css header

--- footer.php
+++ footer.php
@@ -1,29 +1,25 @@
 <footer>
-    <div class="list-header">
-        <ul>
-            <li><a href="">
-                    Accueil
-                </a>
-            </li>
-            <li><a href="">
-                    Boutique
-                </a>
-            </li>
-            <li><a href="">
-                    Calendrier
-                </a>
-            </li>
-            <?php if (!isset($_SESSION['id'])) { ?>
-                <li><a href="inscription.php">Inscription</a></li>
-                <li><a href="connexion.php">Connexion</a></li>
-            <?php } else { ?>
-                <li><a href="disconnect.php">Déconnexion</a></li>
-            <?php } ?>
-            <div class="social">
-                <li><a href=""> <i class="fab fa-instagram"></i></a></li>
-                <li><a href=""> <i class="fab fa-facebook"></i></a></li>
-                <li><a href=""> <i class="fab fa-twitter"></i></a></li>
-            </div>
-        </ul>
-
+<div class="list-header">
+            <ul>
+                <li><a href="">
+                        Accueil
+                    </a>
+                </li>
+                <li><a href="">
+                        Boutique
+                    </a>
+                </li>
+                <li><a href="">
+                       Calendrier
+                    </a>
+                </li>
+    <?php if (!isset($_SESSION['id'])) { ?>
+        <a href="inscription.php">Inscription</a>
+        <a href="connexion.php">Connexion</a>
+    <?php } ?>
+    <div class="social">
+    <li><a href=""> <i class="fab fa-instagram"></i></a></li>
+    <li><a href=""> <i class="fab fa-facebook"></i></a></li>
+    <li><a href=""> <i class="fab fa-twitter"></i></a></li>
+</div>
 </footer>
--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <?php
 
 use Base\Profil;
-
 include 'src/Base.php';
 $user = new Base\profil_utilisateurs();
 ?>
@@ -10,79 +9,54 @@ $user = new Base\profil_utilisateurs();
     <div class="big-header">
         <div class="inline-nav-img">
             <nav class="big-nav">
-                <div class="bond-title">
-                    <h1 class="title">FOO2F
-                        <span class="ballon-inner">
-                            <i onMouseOver="" class=" bond far fa-futbol"></i>
-                            <span class="ombre-other">
-                                <span class="ombre"></span></span>
-                            <span class="ballon"></span>
-                        </span>OT
-                    </h1>
-                </div>
+                <div class="bond" onMouseOver=""><h1 class="title">FOO2F<i class="far fa-futbol"></i>OT</h1></div>
                 <?php if (isset($_SESSION['id'])) { ?>
-                <ul>
-                    <div class="center">
+                    <ul>
+                        <div class="center">
                         <?php if ($user->getIsConnect()) {
                             ?>Bonjour <?= $user->prenom ?></li>
                         <?php } else {
                             return false;
                         }
                         ?>
-                    </div>
-                    <div class="dropdown">
-                        <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"><a href=""> <i
-                                        class="fas fa-user"></i></a></button>
-                        <div class="dropdown-menu">
-                            <?php
-                            if ($user->isAdmin()) {
-                                ?>
-                                <a class="dropdown-item" href="admin.php">Panel admin</a>
-                            <?php } ?>
-
-                            <a class="dropdown-item" href="profil_user.php">Mon profil</a>
-                            <a class="dropdown-item" href="disconnect.php">Déconnexion</a>
-                            <a class="dropdown-item" href="#"></a>
                         </div>
-                    </div>
-                    <div class="cart-plus">
+                        <div class="dropdown">
+                            <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"> <a href=""> <i class="fas fa-user"></i></a></button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="admin.php">Mon compte</a>
+                                <a class="dropdown-item" href="disconnect.php">Déconnexion</a>
+                                <a class="dropdown-item" href="#"></a>
+                            </div>
+                        </div>
+                        <div class="cart-plus">
                         <li><a href="panier.php"> <i class="fas fa-cart-plus"></i></a></li>
-                </ul>
+                    </ul>
+                </div>
+            </nav>
+                    <?php
+                }
+                ?>
+            </nav>
         </div>
-        <?php
-        }
-        ?>
-        </nav>
-    </div>
-    <div class="list-header">
-        <ul>
-            <li><a href="index.php">
-                    Accueil
-                </a>
-            </li>
-            <li><a href="boutique.php">
-                    Boutique
-                </a>
-            </li>
-            <li><a href="calendrier.php">
-                    Calendrier
-                </a>
-            </li>
-            <?php if (!isset($_SESSION['id'])) { ?>
-                <li><a href="inscription.php">
-                        Inscription
+        <div class="list-header">
+            <ul>
+                <li><a href="index.php">
+                        Accueil
                     </a>
                 </li>
-                <li><a href="connexion.php">
-                        Connexion
+                <li><a href="boutique.php">
+                        Boutique
                     </a>
                 </li>
-            <?php } ?>
-        </ul>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-dark" type="submit">Search</button>
-        </form>
-    </div>
+                <li><a href="calendrier.php">
+                        Calendrier
+                    </a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-dark" type="submit">Search</button>
+            </form>
+        </div>
     </div>
 </header>
