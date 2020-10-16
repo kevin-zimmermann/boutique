b56df75ffd504ca36a094b0919a46381fea29f32
lea-martel <lea.martel@laplateforme.io>
2020-10-16 17:32:57 +0200

    page boutique.php

---
+++ boutique.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/css/fa.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/css/headerfooter.css">
+    <link rel="stylesheet" href="styles/css/boutique.css">
+    <title> Accueil - Foo2Foot</title>
+</head>
+<body>
+<?php include 'header.php'?>
+<?php
+$product = new Base\product__cat();
+?>
+    <main>
+        <div class="cards-list">
+            <?php foreach ($product->getProducts() as $product) { ?>
+                <div class="card-desk-content">
+                    <div class="card">
+                        <img class="card-img-top" src="data/product_img/<?= $product['produit_id'] ?>.jpg" alt="<?= $product['nom_produit'] ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $product['nom_produit'] ?></h5>
+                            <h5 class="card-title-prix"><?= $product['prix'] ?>€</h5>
+                        </div>
+                        <div class="footer-product">
+                            <p class="card-text"><button type="button" class="btn btn-primary">Voir plus</button></p>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+
+    </main>
+<?php include 'footer.php' ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 
 use Base\Profil;
+
 include 'src/Base.php';
 $user = new Base\profil_utilisateurs();
 $header = new Base\Header();
@@ -11,7 +12,7 @@ $header = new Base\Header();
         <div class="inline-nav-img">
             <nav class="big-nav">
                 <div class="bond-title">
-                    <h1 class="title">FOO2F <a href="index"</a>
+                    <h1 class="title">FOO2F <a href="index"></a>
                         <span class="ballon-inner">
                             <i onMouseOver="" class=" bond far fa-futbol"></i>
                             <span class="ombre-other">
@@ -21,63 +22,67 @@ $header = new Base\Header();
                     </h1>
                 </div>
                 <?php if (isset($_SESSION['id'])) { ?>
-                    <ul>
-                        <div class="center">
+                <ul>
+                    <li class="center">
                         <?php if ($user->getIsConnect()) {
-                            ?>Bonjour <?= $user->prenom ?></li>
-                        <?php } else {
-                            return false;
-                        }
-                        ?>
+                        ?>Bonjour <?= $user->prenom ?>
+                    <?php } else {
+                        return false;
+                    }
+                    ?>
+                    </li>
+                    <li class="dropdown-marge dropdown">
+                        <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"><a href=""> <i
+                                        class="fas fa-user"></i></a></button>
+                        <div class="dropdown-menu">
+                            <?php
+                            if ($user->isAdmin()) {
+                                ?>
+                                <a class="dropdown-item" href="admin.php">Panel admin</a>
+                            <?php } ?>
+                            <a class="dropdown-item" href="admin.php">Mon profil</a>
+                            <a class="dropdown-item" href="disconnect.php">Déconnexion</a>
+                            <a class="dropdown-item" href="#"></a>
                         </div>
-                        <div class="dropdown-marge dropdown">
-                            <button class="btn btn-dark dropdown-toggle" type="button" data-toggle="dropdown"> <a href=""> <i class="fas fa-user"></i></a></button>
-                            <div class="dropdown-menu">
-                                <?php
-                                if ($user->isAdmin()) {
-                                    ?>
-                                    <a class="dropdown-item" href="admin.php">Panel admin</a>
-                                <?php } ?>
-                                <a class="dropdown-item" href="admin.php">Mon profil</a>
-                                <a class="dropdown-item" href="disconnect.php">Déconnexion</a>
-                                <a class="dropdown-item" href="#"></a>
-                            </div>
+                    </li>
+                    <li class="cart-plus">
+                        <a href="panier.php">
+                            <i class="fas fa-cart-plus"></i>
+                        </a>
+                    </li>
+                </ul>
+        </div>
+        </nav>
+        <?php
+        }
+        ?>
+        </nav>
+    </div>
+    <div class="list-header">
+        <ul>
+            <li><a href="index.php">
+                    Accueil
+                </a>
+            </li>
+            <li class="dropdown">
+                <a data-toggle="dropdown" href="boutique.php">Boutique</a>
+                <div class="dropdown-menu">
+                    <?php foreach ($header->getCategories() as $categorie) { ?>
+                        <div class="dropdown-item">
+                            <a href="boutique.php"><?= $categorie['nom_categorie'] ?></a>
                         </div>
-                        <div class="cart-plus">
-                        <li><a href="panier.php"> <i class="fas fa-cart-plus"></i></a></li>
-                    </ul>
+                    <?php } ?>
                 </div>
-            </nav>
-                    <?php
-                }
-                ?>
-            </nav>
-        </div>
-        <div class="list-header">
-            <ul>
-                <li><a href="index.php">
-                        Accueil
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <p data-toggle="dropdown">Boutique</p>
-                    <div class="dropdown-menu">
-                        <?php foreach ($header->getCategories() as $categorie) { ?>
-                        <div class="dropdown-item" >
-                            <?= $categorie['nom_categorie'] ?>
-                        </div>
-                        <?php } ?>
-                    </div>
-                </li>
-                <li><a href="calendrier.php">
-                        Calendrier
-                    </a>
-                </li>
-            </ul>
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-dark" type="submit">Search</button>
-            </form>
-        </div>
+            </li>
+            <li><a href="calendrier.php">
+                    Calendrier
+                </a>
+            </li>
+        </ul>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-dark" type="submit">Search</button>
+        </form>
+    </div>
     </div>
 </header>
