c6ac9e59a24ccc7207d1be447c69fbbf51258a5e
lea-martel <lea.martel@laplateforme.io>
2020-10-21 17:26:11 +0200

    add product cart

--- product.php
+++ product.php
@@ -6,9 +6,7 @@
     <link rel="stylesheet" href="styles/css/fa.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
@@ -26,11 +24,10 @@
 <?php
 $product = new Base\product__cat();
 $product = $product->setProduct();
-$size = $product->getSizes();
+$sizes = $product->getSizes();
 ?>
 <main>
     <div class="cards-list">
-        <?php foreach ($product-> setProduct() as $product) { ?>
         <div class="card-desk-content">
             <div class="card">
                 <img class="card-img-top" src="data/product_img/<?= $product->produit_id ?>.jpg"
@@ -38,27 +35,31 @@ $size = $product->getSizes();
                 <div class="card-body">
                     <h5 class="card-title"><?= $product->nom_produit ?></h5>
                     <p><?= $product->description ?></p>
-                    <form action="action.php" id="form" class="form form-ajax-other" method="post">
+                    <form action="action.php?product_id=<?= $product->produit_id ?>" id="form" class="form form-ajax-other form-ajax" method="post">
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Choisir taille:</label>
-                            <select class="form-control" id="exampleFormControlSelect1" class="input">
-                                <option value="s">S</option>
-                                <option value="m">M</option>
-                                <option value="l">L</option>
-                                <option value="xl">XL</option>
+                            <select class="form-control input change-input" name="size" id="exampleFormControlSelect1">
+                                <?php
+                                $i = 0;
+                                $stock = 0;
+                                foreach ($sizes as $size) {
+                                    if($i == 0)
+                                    {
+                                        $stock = $size['stock'];
+                                    }
+                                    ++$i;
+                                    ?>
+                                <option value="<?= $size['taille'] ?>" data-value-stock="<?= $size['stock'] ?>"><?= strtoupper($size['taille']) ?></option>
+                                <?php } ?>
                             </select>
-                            <?php } ?>
                         </div>
-                        <?php
-                        var_dump($product);
-                        ?>
 
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Choisir quantité:</label>
-                            <select class="form-control" id="exampleFormControlSelect1" class="input">
+                            <select class="form-control input change-input-value" name="stock" id="exampleFormControlSelect1" >
                                 <?php
                                 $i = 0;
-                                while ($i < 30) {
+                                while ($i < $stock) {
                                     $i++; ?>
                                     <option value="<?= $i ?>"><?= $i ?></option>
                                 <?php } ?>
@@ -68,7 +69,7 @@ $size = $product->getSizes();
                 </div>
                 <a class="footer-product">
                     <input type="hidden" name="type" value="panierAdd" class="input">
-                    <a href="addpanier.php?produit_id=<?= $product->produit_id ?>"<button type="submit" class="btn btn-primary">Ajouter au panier</button></a>
+                    <button type="submit" class="btn btn-primary">Ajouter au panier</button>
                 </div>
                 </form>
             </div>
@@ -80,3 +81,21 @@ $size = $product->getSizes();
 <?php include 'footer.php' ?>
 </body>
 </html>
+<script>
+    function getOption(stock)
+    {
+        let html = '';
+        for (let i = 1; i <= stock; ++i)
+        {
+            html += '<option value="' + i + '">' + i + '</option>';
+        }
+        return html;
+    }
+    $('.change-input').change(function () {
+        let value = $(this).val();
+        let stock = $(this).find('[value=' + value + ']')
+        $('.change-input-value').html(getOption(stock.data('value-stock')));
+    });
+
+</script>
+<script src="script.js"></script>
