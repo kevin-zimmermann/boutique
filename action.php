8e2d99e72577575653ce1b346abe2a4d5e8610a9
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2020-10-21 12:01:02 +0200

    adress -> good

--- action.php
+++ action.php
@@ -4,23 +4,27 @@
 $url = "";
 include 'src/Base.php';
 $error = [];
+$return = [];
 if(in_array($_POST['type'], ['inscription', 'connexion']))
 {
     $account = new Base\register_connexion();
     if ($_POST['type'] == "inscription") {
         $url = "connexion.php";
         $error = $account->register();
+        $return = [$url, $error];
     }
 
     if ($_POST['type'] == "connexion") {
         $url = "index.php";
         $error = $account->connexion();
+        $return = [$url, $error];
     }
 }
 if($_POST['type'] == "modif_password"){
     $profil = new Base\profil_utilisateurs;
-    $url = "profil.php";
+    $url = "profil_password.php";
     $error = $profil->register();
+    $return = [$url, $error];
 }
 
 if($_POST['type'] == "change_profil_email")
@@ -28,9 +32,31 @@ if($_POST['type'] == "change_profil_email")
     $profil = new Base\profil_utilisateurs;
     $url = "profil_user.php";
     $error = $profil->change();
+    $return = [$url, $error];
 
 }
+if ($_POST['type'] == "addadress")
+{
+    $profil = new Base\profil_utilisateurs;
+    $url = "profil_adresse.php";
+    $error = $profil->addAdress();
+    $return = [$url, $error];
 
-echo json_encode([$url, $error]);
+}
+if($_POST['type'] == 'editAddress')
+{
+    $profil = new Base\profil_utilisateurs;
+    $url = "profil_adresse.php";
+    $error = $profil->editAddres();
+    $return = [$url, $error];
+}
+if ($_POST['type'] == "deleteAddress")
+{
+    $profil = new Base\profil_utilisateurs;
+    $url = "profil_adresse.php";
+    $error = $profil->deleteAdress();
+    $return = ['url' => $url, 'return' => $error];
+}
+echo json_encode($return);
 
 
