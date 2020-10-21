a7df07fdb76ea49b98c850e46ef359f63267fcd1
lea-martel <lea.martel@laplateforme.io>
2020-10-21 14:40:18 +0200

    panier

--- product.php
+++ product.php
@@ -6,7 +6,9 @@
     <link rel="stylesheet" href="styles/css/fa.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
@@ -24,7 +26,7 @@
 <?php
 $product = new Base\product__cat();
 $product = $product->setProduct();
-$sizes = $product->getSizes();
+$size = $product->getSizes();
 ?>
 <main>
     <div class="cards-list">
@@ -36,32 +38,27 @@ $sizes = $product->getSizes();
                 <div class="card-body">
                     <h5 class="card-title"><?= $product->nom_produit ?></h5>
                     <p><?= $product->description ?></p>
-                    <form action="action.php?product_id=<?= $product->produit_id ?>" id="form" class="form form-ajax-other form-ajax" method="post">
+                    <form action="action.php" id="form" class="form form-ajax-other" method="post">
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Choisir taille:</label>
-                            <select class="form-control input change-input" name="size" id="exampleFormControlSelect1">
-                                <?php
-                                $i = 0;
-                                $stock = 0;
-                                foreach ($sizes as $size) {
-                                    if($i == 0)
-                                    {
-                                        $stock = $size['stock'];
-                                    }
-                                    ++$i;
-                                    ?>
-                                <option value="<?= $size['taille'] ?>" data-value-stock="<?= $size['stock'] ?>"><?= strtoupper($size['taille']) ?></option>
-                                <?php } ?>
+                            <select class="form-control" id="exampleFormControlSelect1" class="input">
+                                <option value="s">S</option>
+                                <option value="m">M</option>
+                                <option value="l">L</option>
+                                <option value="xl">XL</option>
                             </select>
                             <?php } ?>
                         </div>
+                        <?php
+                        var_dump($product);
+                        ?>
 
                         <div class="form-group">
                             <label for="exampleFormControlSelect1">Choisir quantité:</label>
-                            <select class="form-control input change-input-value" name="stock" id="exampleFormControlSelect1" >
+                            <select class="form-control" id="exampleFormControlSelect1" class="input">
                                 <?php
                                 $i = 0;
-                                while ($i < $stock) {
+                                while ($i < 30) {
                                     $i++; ?>
                                     <option value="<?= $i ?>"><?= $i ?></option>
                                 <?php } ?>
@@ -83,21 +80,3 @@ $sizes = $product->getSizes();
 <?php include 'footer.php' ?>
 </body>
 </html>
-<script>
-    function getOption(stock)
-    {
-        let html = '';
-        for (let i = 1; i <= stock; ++i)
-        {
-            html += '<option value="' + i + '">' + i + '</option>';
-        }
-        return html;
-    }
-    $('.change-input').change(function () {
-        let value = $(this).val();
-        let stock = $(this).find('[value=' + value + ']')
-        $('.change-input-value').html(getOption(stock.data('value-stock')));
-    });
-
-</script>
-<script src="script.js"></script>
