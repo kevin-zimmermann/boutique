4126b79a9d4bd1110905caa76a52668556629069
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2020-10-22 16:43:37 +0200

    SEARCH BAR -> index.php

--- index.php
+++ index.php
@@ -7,9 +7,6 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
@@ -23,7 +20,7 @@
     <title> Accueil - Foo2Foot</title>
 </head>
 <body>
-<?php include 'header.php'?>
+<?php include 'header.php' ?>
 <?php
 $product = new Base\product__cat();
 ?>
