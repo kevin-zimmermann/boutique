c365178fd29342b493eb3ecc417db2f6331182d0
kevin-zimmermann <kevin.zimmermann@laplateforme.io>
2020-10-23 11:46:58 +0200

    delete product in Cart.php

--- Cart.php
+++ Cart.php
@@ -6,9 +6,7 @@
     <link rel="stylesheet" href="styles/css/fa.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
@@ -40,23 +38,55 @@ $product= new \Base\product__cat();
                             <th scope="col">Quantité</th>
                             <th scope="col">Prix</th>
                             <th scope="col">Supprimer</th>
-                            <th scope="col">Modifier</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <?php foreach ($carts->getPanier() as $cart) { ?>
+                        <?php foreach ($carts->getPanier() as $cart) {
+
+                            $stock = $carts->setSize($cart['size'], $cart['produit_id']);
+                            ?>
                             <tr class="table-ajax">
                                 <td><img width="49px" src="data/product_img/<?= $cart['produit_id'] ?>.jpg" alt="<?= $cart['nom_produit'] ?>"></td>
                                 <td><?= $cart['nom_produit'] ?></td>
                                 <td><?= strtoupper($cart['size'])?></td>
-                                <td><?= $cart['quantity'] ?></td>
+                                <td><select class="qte" data-product-id="<?= $cart['produit_id'] ?>" data-size="<?= $cart['size'] ?>">
+                                        <?php for ($i = 1; $i <= $stock->stock; ++$i) {?>
+                                            <option value="<?= $i ?>" <?= $i == $cart['quantity'] ? 'selected' : '' ?>>
+                                                <?= $i ?>
+                                            </option>
+                                        <?php } ?>
+                                    </select></td>
                                 <td><?= $cart['prix'] ?></td>
-                                <td class="ajax-delete" data-id="<?= $cart['produit_id'] ?>" data-name="product_id"><i
+                                <td class="ajax-delete" data-id="<?= $cart['panier_id'] ?>" data-name="panier_id"><i
                                             class="fas fa-trash"></i></td>
-                                <td><a href="admin_modif_product.php?produit_id=<?= $cart['produit_id'] ?>"><i
-                                                class="fas fa-pen"></i></a></td>
                             </tr>
                         <?php } ?>
+                        <div class="get-delete get-popup">
+                            <div class="get-delete-inner get-popup-inner">
+                                <h3>Confirmation <a class="overlay-popup close-popup-delete" href=""></a></h3>
+                                <div class="content-delete">
+                                    blabla
+                                </div>
+                                <div class="conf">
+                                    <form action="action.php" class="action-ajax" method="post">
+                                        <input type="hidden" name="type" value="deleteProductcart">
+                                        <input type="hidden" class="action-input-hidden">
+                                        <button class="btn btn-primary">button</button>
+                                    </form>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="get-error get-popup">
+                            <div class="get-error-inner r get-popup-inner">
+                                <h3>Oops il y a une erreur <a class="overlay-popup close-popup-error" href=""></a></h3>
+                                <div class="content-error">
+                                    blabla
+                                </div>
+                            </div>
+
+                        </div>
 
                         </tbody>
                     </table>
@@ -68,3 +98,64 @@ $product= new \Base\product__cat();
 <?php include 'footer.php' ?>
 </body>
 </html>
+<script>
+    function leavePopup(getPopup) {
+        getPopup.animate({opacity: 0}, {duration: 100}).delay(100).queue(function (next) {
+            $(this).removeClass('active-overlay');
+            next();
+        })
+    }
+
+    $('.ajax-delete').click(function () {
+        $('.get-delete').addClass('active-overlay');
+        $('.get-delete').animate({opacity: 1}, {duration: 100});
+        $('.get-delete').find('.action-input-hidden')
+            .attr('name', $(this).data('name'))
+            .val($(this).data('id'));
+    })
+    $('.action-ajax').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: $(this).attr('action'),
+            method: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: (data) => {
+                $('[data-id=' + data['return'] + ']').closest('.table-ajax').remove();
+                leavePopup($('.get-delete'));
+            },
+            error: (error) => {
+                console.log(error.responseText)
+            }
+        });
+        return false;
+    });
+    $('.get-popup').click(function (e) {
+        let div = $(this).find('.get-popup-inner');
+        if (!$(e.target).is(div) && !$.contains(div[0], e.target)) {
+            leavePopup($(this));
+        }
+    });
+    $('.overlay-popup').click(function () {
+        leavePopup($(this).closest('.get-popup'));
+        return false;
+    });
+    $('.qte').change(function () {
+        let value = $(this).val()
+        let productId = $(this).data('product-id');
+        let size = $(this).data('size');
+        $.ajax({
+            url : 'size.php',
+            method : 'POST',
+            data : {
+                productId : productId,
+                size : size,
+                value : value
+            },
+            success : (data) =>{
+
+            }
+
+        })
+    })
+</script>
